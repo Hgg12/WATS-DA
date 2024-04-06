3b76efe704ee8643f35c50dab9be297b1dee0f1b
HGG12 <2607717029@qq.com>
2024-04-06 19:56:42 +0800

    1

--- pysot/core/config.py
+++ pysot/core/config.py
@@ -138,7 +138,7 @@ __C.DATASET.GRAY = 0.0
 # __C.DATASET.NAMES = ('VID', 'COCO', 'DET', 'YOUTUBEBB')
 
 __C.DATASET.SOURCE = ['TrackingNet'] # only train on COCO for test  'VID', , 'DET', 'YOUTUBEBB' , 'COCO', 'YOUTUBEBB'
-__C.DATASET.TARGET = ['WATB400_1','WATB400_2','WATB2024_3']  # only train on COCO for test  'VID', , 'DET', 'YOUTUBEBB'
+__C.DATASET.TARGET = ['Wildlife2024_1','Wildlife2024_2']  # only train on COCO for test  'VID', , 'DET', 'YOUTUBEBB'
 
 __C.DATASET.VID = CN()
 __C.DATASET.VID.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/vid/crop511'
@@ -152,23 +152,17 @@ __C.DATASET.TrackingNet.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracke
 __C.DATASET.TrackingNet.FRAME_RANGE = 100
 __C.DATASET.TrackingNet.NUM_USE = -1 # 10000  # repeat until reach NUM_USE
 
-__C.DATASET.WATB400_1= CN()
-__C.DATASET.WATB400_1.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/WATB400-1/crop511'
-__C.DATASET.WATB400_1.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/WATB400-1/result/hqsam_WATB400-1.json'
-__C.DATASET.WATB400_1.FRAME_RANGE = 1
-__C.DATASET.WATB400_1.NUM_USE = -1 # 10000
+__C.DATASET.Wildlife2024_1= CN()
+__C.DATASET.Wildlife2024_1.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/Wildlife2024_1/crop511'
+__C.DATASET.Wildlife2024_1.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/Wildlife2024_1/result/hqsam_Wildlife2024_1.json'
+__C.DATASET.Wildlife2024_1.FRAME_RANGE = 1
+__C.DATASET.Wildlife2024_1.NUM_USE = -1 # 10000
 
-__C.DATASET.WATB400_2= CN()
-__C.DATASET.WATB400_2.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/WATB400-2/crop511'
-__C.DATASET.WATB400_2.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/WATB400-2/result/hqsam_WATB400-2.json'
-__C.DATASET.WATB400_2.FRAME_RANGE = 1
-__C.DATASET.WATB400_2.NUM_USE = -1 # 10000
-
-__C.DATASET.WATB2024_3= CN()
-__C.DATASET.WATB2024_3.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/WATB2024_3/crop511'
-__C.DATASET.WATB2024_3.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/WATB2024_3/result/hqsam_WATB2024_3.json'
-__C.DATASET.WATB2024_3.FRAME_RANGE = 1
-__C.DATASET.WATB2024_3.NUM_USE = -1 # 10000
+__C.DATASET.Wildlife2024_2= CN()
+__C.DATASET.Wildlife2024_2.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/Wildlife2024_2/crop511'
+__C.DATASET.Wildlife2024_2.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/Wildlife2024_2/result/hqsam_Wildlife2024_2.json'
+__C.DATASET.Wildlife2024_2.FRAME_RANGE = 1
+__C.DATASET.Wildlife2024_2.NUM_USE = -1 # 10000
 
 __C.DATASET.YOUTUBEBB = CN()
 __C.DATASET.YOUTUBEBB.ROOT = 'train_dataset/yt_bb/crop511'  # YOUTUBEBB dataset path
@@ -182,12 +176,6 @@ __C.DATASET.COCO.ANNO = 'train_dataset/coco/train2017.json'
 __C.DATASET.COCO.FRAME_RANGE = 1
 __C.DATASET.COCO.NUM_USE = -1
 
-__C.DATASET.DET = CN()
-__C.DATASET.DET.ROOT = 'train_dataset/det/crop511'           # DET dataset path
-__C.DATASET.DET.ANNO = 'train_dataset/det/train.json'
-__C.DATASET.DET.FRAME_RANGE = 1
-__C.DATASET.DET.NUM_USE = -1
-
 __C.DATASET.GOT10K = CN()
 __C.DATASET.GOT10K.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/crop511'
 __C.DATASET.GOT10K.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/got10k/train.json'
@@ -199,60 +187,6 @@ __C.DATASET.LaSOT.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/
 __C.DATASET.LaSOT.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/lasot/train.json'
 __C.DATASET.LaSOT.FRAME_RANGE = 100
 __C.DATASET.LaSOT.NUM_USE = 100000
-
-__C.DATASET.DarkTrack = CN()
-__C.DATASET.DarkTrack.ROOT = '/home/mist/v4r/train_dataset/random_train/random_crop511' # '/home/mist/v4r/train_dataset/NAT2021-train/crop_511'         # GOT dataset path
-__C.DATASET.DarkTrack.ANNO = '/home/mist/v4r/train_dataset/random_train/train_random.json' # '/home/mist/v4r/train_dataset/NAT2021-train/train.json'
-__C.DATASET.DarkTrack.FRAME_RANGE = 50
-__C.DATASET.DarkTrack.NUM_USE = 10000 # 100000
-
-__C.DATASET.NAT = CN()
-__C.DATASET.NAT.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/sam_nat/crop511'
-__C.DATASET.NAT.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/sam_nat/sam_nat_b.json'
-__C.DATASET.NAT.ANNO_B = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/sam_nat/sam_nat_b.json'
-__C.DATASET.NAT.ANNO_S = './train_dataset/sam_nat/sam_nat_s.json'
-__C.DATASET.NAT.ANNO_T = './train_dataset/sam_nat/sam_nat_t.json'
-__C.DATASET.NAT.ANNO_N = './train_dataset/sam_nat/sam_nat_n.json'
-__C.DATASET.NAT.FRAME_RANGE = 1
-__C.DATASET.NAT.NUM_USE = -1 # 10000
-
-__C.DATASET.ExDark = CN()
-__C.DATASET.ExDark.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/ExDark/crop511'
-__C.DATASET.ExDark.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/ExDark/sam_exdark.json'
-__C.DATASET.ExDark.FRAME_RANGE = 1
-__C.DATASET.ExDark.NUM_USE = -1 # 10000
-
-__C.DATASET.wild = CN()
-__C.DATASET.wild.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/wild2/crop511'
-__C.DATASET.wild.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/wild2/result/hqsam_wild.json'
-__C.DATASET.wild.FRAME_RANGE = 1
-__C.DATASET.wild.NUM_USE = -1 # 10000
-
-__C.DATASET.WATB_50 = CN()
-__C.DATASET.WATB_50.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/WATB_50/crop511'
-__C.DATASET.WATB_50.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/WATB_50/train.json'
-__C.DATASET.WATB_50.FRAME_RANGE = 1
-__C.DATASET.WATB_50.NUM_USE = -1 # 10000
-
-
-
-__C.DATASET.DarkFace = CN()
-__C.DATASET.DarkFace.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/DarkFace/crop511'
-__C.DATASET.DarkFace.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/DarkFace/result/sam_DarkFace.json'
-__C.DATASET.DarkFace.FRAME_RANGE = 1
-__C.DATASET.DarkFace.NUM_USE = -1 # 10000
-
-__C.DATASET.Animals = CN()
-__C.DATASET.Animals.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/TEST/crop511'
-__C.DATASET.Animals.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/TEST/result/hqsam_animals.json'
-__C.DATASET.Animals.FRAME_RANGE = 1
-__C.DATASET.Animals.NUM_USE = -1 # 10000
-
-__C.DATASET.UAV = CN()
-__C.DATASET.UAV.ROOT = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/UAV/crop511'
-__C.DATASET.UAV.ANNO = '/media/w/719A549756118C56/HGG/SAM-DA-main/tracker/BAN/train_dataset/UAV/train.json'
-__C.DATASET.UAV.FRAME_RANGE = 1
-__C.DATASET.UAV.NUM_USE = -1 # 10000
 
 __C.DATASET.VIDEOS_PER_EPOCH = 20000
 __C.DATASET.VIDEOS_PER_EPOCH_B = 20000
@@ -362,20 +296,6 @@ __C.TRACK.REGION_L = 0.44
 __C.HP_SEARCH = CN()
 
 __C.HP_SEARCH.UAV123 = [0.39, 0.04, 0.37] 
-
-__C.HP_SEARCH.NAT = [0.39, 0.04, 0.37] 
-
-__C.HP_SEARCH.NAT_L = [0.39, 0.04, 0.37] 
-
-__C.HP_SEARCH.UAVDark70 = [0.32, 0.04, 0.36] 
-
-__C.HP_SEARCH.NUT_L = [0.473, 0.02, 0.385]
-
-# __C.HP_SEARCH.NUT_L = [0.39, 0.04, 0.37] 
-
-__C.HP_SEARCH.DarkTrack2021 = [0.473, 0.06, 0.305]
-
-__C.HP_SEARCH.UAVDark135 = [0.473, 0.06, 0.305]
 
 __C.HP_SEARCH.WATB = [0.39, 0.04, 0.37] 
 
